ca42520200cc816b3983af063b038ef274cb01e6
jehyungmin <wpaudrl@gmail.com>
2019-03-11 07:24:47 +0900

    commit

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -3,7 +3,8 @@ import EditorTemplate from '../components/editor/EditorTemplate';
 import EditorHeader from '../components/editor/EditorHeader';
 import EditorPane from '../components/editor/EditorPane';
 import PreviewPane from '../components/editor/PreviewPane';
-import EditorPaneContainer from '../components/editor/EditorPaneContainer';
+import PreviewPaneContainer from '../containers/editor/PreviewPaneContainer';
+import EditorPaneContainer from '../containers/editor/EditorPaneContainer';
 
 const EditorPage = () => {
     return (
@@ -11,7 +12,7 @@ const EditorPage = () => {
             <EditorTemplate 
                 header={<EditorHeader/>}
                 editor={<EditorPaneContainer/>}
-                preview={<PreviewPane />}
+                preview={<PreviewPaneContainer/>}
             />
         </div>
     )
