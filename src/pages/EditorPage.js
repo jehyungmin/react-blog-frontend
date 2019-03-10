6c4f7ddb719b39abfe9add8b7a5b3d8d1a93a738
jehyungmin <wpaudrl@gmail.com>
2019-03-11 05:05:44 +0900

    commit

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -3,13 +3,14 @@ import EditorTemplate from '../components/editor/EditorTemplate';
 import EditorHeader from '../components/editor/EditorHeader';
 import EditorPane from '../components/editor/EditorPane';
 import PreviewPane from '../components/editor/PreviewPane';
+import EditorPaneContainer from '../components/editor/EditorPaneContainer';
 
 const EditorPage = () => {
     return (
         <div>
             <EditorTemplate 
                 header={<EditorHeader/>}
-                editor={<EditorPane/>}
+                editor={<EditorPaneContainer/>}
                 preview={<PreviewPane />}
             />
         </div>
